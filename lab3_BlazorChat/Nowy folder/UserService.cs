483f73d398d7696fd253a7dd1ec0930848492298
stanislav.podmazov <stanislav.podmazov@BR-24>
2023-03-15 10:28:25 +0100

    Dodaj pliki projektów.

---
+++ lab3_BlazorChat/Nowy folder/UserService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authentication.OAuth.Claims;
+using Microsoft.Extensions.Configuration.UserSecrets;
+
+namespace lab3_BlazorChat.Nowy_folder
+{
+    public class UserService
+    {
+        public void Add(string userId, string username)
+        {
+            Add(userId, username);
+        }
+        public void RemoveByName(string _username)
+        {
+             
+        }
+        //public string GetConnectionIdByName(string username)
+        //{
+            
+        //}
+     
+
+    }
+}
